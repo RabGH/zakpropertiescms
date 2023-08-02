066ecd4e1eb93ad26f3e2be570a18004db97f9e3
RabGh <rabiighais@gmail.com>
2023-08-02 04:21:52 +0300

    Organize new backend schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,17 +1,21 @@
 import property from './projects/property'
-import propertyImage from './projects/propertyImage'
+import propertyImage from './projects/propertyImages'
 import project from './projects/project'
 import projectImage from './projects/projectImages'
 import address from './projects/address'
 import amenities from './projects/amenities'
 import developer from './projects/developer'
+import paymentPlans from './projects/paymentPlans'
+import developerImages from './projects/developerImages'
 
 export const schemaTypes = [
   property,
   propertyImage,
   project,
   projectImage,
+  developer,
+  developerImages,
   address,
   amenities,
-  developer,
+  paymentPlans,
 ]
