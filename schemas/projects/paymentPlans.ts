7d385ed8256236e83e8769da89270f5824608654
RabGh <rabiighais@gmail.com>
2023-08-02 04:23:47 +0300

    Add fields & separate offplan logic & add payment

--- schemas/projects/paymentPlans.ts
+++ schemas/projects/paymentPlans.ts
@@ -7,6 +7,18 @@ export default {
       name: 'name',
       title: 'Payment Plan',
       type: 'string',
+      description: 'The name of the payment plan, e.g. "10% Down Payment Plan".',
+    },
+    {
+      name: 'createdAt',
+      title: 'Created At',
+      type: 'datetime',
+      options: {
+        dateFormat: 'YYYY-MM-DD',
+        timeFormat: 'HH:mm:ss',
+        inputUtc: true,
+      },
+      description: 'This is the date the payment plan was added to the database.',
     },
     {
       name: 'type',
@@ -22,16 +34,20 @@ export default {
         ],
         layout: 'radio',
       },
+      description: 'The type of the payment plan, e.g. "CLP Plan".',
     },
     {
       name: 'reference',
       title: 'Payment Plan Reference',
       type: 'string',
+      description: 'The reference number or ID of the payment plan, e.g. "PP-001".',
     },
     {
       name: 'description',
       title: 'Payment Plan Description',
       type: 'text',
+      description:
+        'The description of the payment plan, e.g. what it covers, what are the terms and conditions, etc.',
     },
     {
       name: 'validity',
@@ -54,6 +70,8 @@ export default {
           type: 'boolean',
         },
       ],
+      description:
+        'The validity of the payment plan, e.g. how long it is valid for, when it expires, or if it is subject to change.',
     },
     {
       name: 'timeline',
@@ -76,6 +94,8 @@ export default {
           ],
         },
       ],
+      description:
+        'The timeline of the payment plan, e.g. when and how much money is due at each stage of the property transaction.',
     },
     {
       name: 'amountType',
@@ -88,21 +108,28 @@ export default {
         ],
         layout: 'radio',
       },
+      description:
+        'The type of the amount that is paid at each timeline item, e.g. absolute or percentage.',
     },
     {
       name: 'amountAbsolute',
       title: 'Amount Absolute (AED)',
       type: 'number',
+      description:
+        'The amount in AED that is paid at each timeline item, if absolute is chosen as the amount type.',
     },
     {
       name: 'amountPercentage',
       title: 'Amount Percentage (%)',
       type: 'number',
+      description:
+        'The percentage of the total price that is paid at each timeline item, if percentage is chosen as the amount type.',
     },
     {
       name: 'interestRate',
       title: 'Interest Rate (%)',
       type: 'number',
+      description: 'The interest rate that is charged on the payment amount, if any.',
     },
     {
       name: 'penalty',
@@ -120,6 +147,7 @@ export default {
           type: 'text',
         },
       ],
+      description: 'The penalty that is imposed on the user if they fail to pay on time, if any.',
     },
   ],
 }
